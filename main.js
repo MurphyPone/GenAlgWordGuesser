40ff573068e016b2edf4425cd30f304bb1dc5449
MurphyPOne <MurphyP1@bishopireton.org>
2018-11-14 10:30:22 -0500

    ripped from danny shiffy for modification in WotD GA

---
+++ main.js
@@ -0,0 +1,71 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+//Modified to accept WotD, definitions from Dict.com
+
+let target;
+let popmax;
+let mutationRate;
+let population;
+
+let bestPhrase;
+let allPhrases;
+let stats;
+
+function setup() {
+  bestPhrase = createP("Best phrase:");
+  //bestPhrase.position(10,10);
+  bestPhrase.class("best");
+
+  allPhrases = createP("All phrases:");
+  allPhrases.position(600, 10);
+  allPhrases.class("all");
+
+  stats = createP("Stats");
+  //stats.position(10,200);
+  stats.class("stats");
+
+  //createCanvas(640, 360);
+  target = "catastrophize";
+  popmax = 200;
+  mutationRate = 0.01;
+
+  // Create a population with a target phrase, mutation rate, and population max
+  population = new Population(target, mutationRate, popmax);
+}
+
+function draw() {
+  // Generate mating pool
+  population.naturalSelection();
+  //Create next generation
+  population.generate();
+  // Calculate fitness
+  population.calcFitness();
+
+  population.evaluate();
+
+  // If we found the target phrase, stop
+  if (population.isFinished()) {
+    //println(millis()/1000.0);
+    noLoop();
+  }
+
+  displayInfo();
+}
+
+function displayInfo() {
+  // Display current status of population
+  let answer = population.getBest();
+
+  bestPhrase.html("Best phrase:<br>" + answer);
+
+  let statstext = "total generations:     " + population.getGenerations() + "<br>";
+  statstext += "average fitness:       " + floor(population.getAverageFitness()*100) + "%<br>";
+  statstext += "total population:      " + popmax + "<br>";
+  statstext += "mutation rate:         " + floor(mutationRate * 100) + "%";
+
+  stats.html(statstext);
+
+  allPhrases.html("All phrases:<br>" + population.allPhrases())
+}
